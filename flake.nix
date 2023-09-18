dd83666b9bb6a541a54773497681d982bd9f7652
Álan Crístoffer <acristoffers@startmail.com>
2023-09-18 14:21:47 +0200

    chore: adds flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "I/O Communication Library";
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        dbkp = pythonPkgs: pythonPkgs.buildPythonPackage rec {
+          format = "pyproject";
+          name = "dbkp";
+          src = ./.;
+          propagatedBuildInputs = with pythonPkgs; [
+            pydantic
+            poetry-core
+          ];
+        };
+        packages.default = dbkp pkgs.python311Packages;
+      });
+}
